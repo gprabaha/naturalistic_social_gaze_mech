5236876a833f26e5a97365b4f38746575e9532ff
Prabaha <prabaha.gangopadhyay@yale.edu>
2024-09-18 14:41:47 -0400

    move all commands from analyze signals script to data manager but they need updating

--- data_manager.py
+++ data_manager.py
@@ -8,6 +8,8 @@ Created on Wed Sep 18 14::51:42 2024
 
 import logging
 
+import util
+
 class DataManager:
     def __init__(self, params):
         self.params = params
@@ -17,4 +19,45 @@ class DataManager:
         """Setup the logger for the DataManager."""
         self.logger = logging.getLogger(__name__)
 
-        
+    def initialize_params(self):
+        _, params = util.get_root_data_dir(params)
+        _, params = util.fetch_processed_data_dir(params)
+
+    def get_data(self):
+        return 0
+
+    def run(self):
+        self.get_data()
+
+
+
+params = {}
+params.update({
+    'processed_data_dir': 'intermediates',
+    'is_cluster': True,
+    'use_parallel': True,
+    'extract_postime_from_mat_files': False,
+    'compute_fixations': False,
+    'fixation_detection_method': 'eye_mvm',
+    'sampling_rate': 0.001,
+    'vel_thresh': 10
+    })
+
+
+
+
+if params.get('extract_postime_from_mat_files', False):
+    sorted_position_path_list, m1_gaze_positions, m2_gaze_positions, sorted_time_path_list, time_vecs = \
+        filter_behav.get_gaze_timepos_across_sessions(params)
+else:
+    sorted_position_path_list, m1_gaze_positions, m2_gaze_positions, sorted_time_path_list, time_vecs = \
+        load_data.get_combined_gaze_pos_and_time_lists(params)
+
+print(sorted_position_path_list)
+
+
+pos_pattern = r"(\d{8})_position_(\d+).mat"
+session_infos = [{'session_name': re.match(pos_pattern, os.path.basename(f)).group(1), 'run_number': int(re.match(pos_pattern, os.path.basename(f)).group(2))} for f in sorted_position_path_list]
+
+# Extract fixations and saccades for both monkeys
+fixations_m1, saccades_m1, fixations_m2, saccades_m2 = fix_and_saccades.extract_fixations_for_both_monkeys(params, m1_gaze_positions, m2_gaze_positions, time_vecs, session_infos)
