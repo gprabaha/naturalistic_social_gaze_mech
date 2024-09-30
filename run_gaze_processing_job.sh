a4427f68699bccbab405df79ef2fff1a137aa59f
Prabaha <prabaha.gangopadhyay@yale.edu>
2024-09-30 15:20:24 -0400

    add path to roi rects when populatng params with paths

--- run_gaze_processing_job.sh
+++ run_gaze_processing_job.sh
@@ -3,12 +3,13 @@
 #SBATCH --partition=psych_day
 #SBATCH --time=1:00:00                # 1 hour time limit
 #SBATCH --cpus-per-task=16            # 16 CPUs
-#SBATCH --mem=16G                     # 16GB of memory
+#SBATCH --mem=32G                     # 16GB of memory
 #SBATCH --output=gaze_processing.out  # Output file
 #SBATCH --error=gaze_processing.err   # Error file
 
 # Load the necessary module and activate the conda environment
 module load miniconda
+conda deactivate
 conda activate gaze_processing
 
 # Run your script
