c83d6f015eb6e209a234432bd79f68b0b19e5401
Prabaha <prabaha.gangopadhyay@yale.edu>
2024-10-01 11:24:37 -0400

    update plotter method to create date subdir for saving plots; update plotter functions to avoid plotting data points in combined plot

--- plotter.py
+++ plotter.py
@@ -10,7 +10,7 @@ def plot_agent_behavior(fixation_data, saccade_data, gaze_data, roi_rects, filen
     # Second subplot: plot saccades
     plot_saccades(axs[1], saccade_data, gaze_data)
     overlay_roi_rects(axs[1], roi_rects)
-    # Third subplot: plot both fixations and saccades (without the 30% opacity points)
+    # Third subplot: plot both fixations and saccades (without 30% opacity scatter points)
     plot_combined(axs[2], fixation_data, saccade_data, gaze_data)
     overlay_roi_rects(axs[2], roi_rects)
     plt.tight_layout()
@@ -19,42 +19,63 @@ def plot_agent_behavior(fixation_data, saccade_data, gaze_data, roi_rects, filen
 
 
 def plot_fixations(ax, fixation_data, gaze_data):
-    # Extract fixation indices and positions
+    # Extract fixation start and stop indices
     fixation_indices = fixation_data['fixationindices']
-    fixation_points = gaze_data[fixation_indices]
-    # Plot fixation points with 30% opacity
-    ax.scatter(fixation_points[:, 0], fixation_points[:, 1], c='black', alpha=0.3)
-    # Plot mean fixation points with color gradient based on relative time
-    mean_fixations = np.mean(fixation_points, axis=1)
-    times = np.arange(len(mean_fixations)) / len(mean_fixations)
-    ax.scatter(mean_fixations[:, 0], mean_fixations[:, 1], c=times, cmap='cool', alpha=1)
+    # Prepare all fixation points and their mean points
+    all_fixation_points = np.vstack([gaze_data[start:end] for start, end in fixation_indices])
+    mean_fixation_points = np.array([np.mean(gaze_data[start:end], axis=0) for start, end in fixation_indices])
+    # Calculate relative time for color
+    relative_times = np.array([start / len(gaze_data) for start, _ in fixation_indices])
+    # Plot all fixation points with 30% opacity
+    ax.scatter(all_fixation_points[:, 0], all_fixation_points[:, 1], c='black', alpha=0.3)
+    # Plot mean fixation points with color gradient
+    ax.scatter(mean_fixation_points[:, 0], mean_fixation_points[:, 1], c=relative_times, cmap='cool', s=100, alpha=1)
 
 
 def plot_saccades(ax, saccade_data, gaze_data):
-    # Extract saccade indices and positions
+    # Extract saccade start and stop indices
     saccade_indices = saccade_data['saccadeindices']
-    saccade_points = gaze_data[saccade_indices]
-    # Plot saccade points with 30% opacity
-    ax.scatter(saccade_points[:, 0], saccade_points[:, 1], c='black', alpha=0.3)
-    # Draw arrows for saccades with color gradient based on relative time
-    start_points = gaze_data[saccade_indices[:, 0]]
-    end_points = gaze_data[saccade_indices[:, 1]]
-    times = np.arange(len(start_points)) / len(start_points)
-    for i, (start, end) in enumerate(zip(start_points, end_points)):
-        ax.arrow(start[0], start[1], end[0] - start[0], end[1] - start[1], 
-                 head_width=0.05, color=plt.cm.viridis(times[i]))
+    # Prepare start and end points of all saccades
+    start_points = np.array([gaze_data[start] for start, _ in saccade_indices])
+    end_points = np.array([gaze_data[end - 1] for _, end in saccade_indices])
+    # Calculate relative time for color
+    relative_times = np.array([start / len(gaze_data) for start, _ in saccade_indices])
+    # Plot all saccade points with 30% opacity
+    all_saccade_points = np.vstack([gaze_data[start:end] for start, end in saccade_indices])
+    ax.scatter(all_saccade_points[:, 0], all_saccade_points[:, 1], c='black', alpha=0.3)
+    # Plot arrows from start to end of saccades with color gradient
+    for i in range(len(start_points)):
+        ax.arrow(start_points[i, 0], start_points[i, 1], 
+                 end_points[i, 0] - start_points[i, 0], 
+                 end_points[i, 1] - start_points[i, 1], 
+                 head_width=0.05, color=plt.cm.viridis(relative_times[i]))
 
 
 def plot_combined(ax, fixation_data, saccade_data, gaze_data):
-    # Plot mean fixation points
-    plot_fixations(ax, fixation_data, gaze_data)
-    # Draw saccade arrows
-    plot_saccades(ax, saccade_data, gaze_data)
+    # Plot mean fixation points (no scatter points)
+    fixation_indices = fixation_data['fixationindices']
+    mean_fixation_points = np.array([np.mean(gaze_data[start:end], axis=0) for start, end in fixation_indices])
+    relative_times_fix = np.array([start / len(gaze_data) for start, _ in fixation_indices])
+    ax.scatter(mean_fixation_points[:, 0], mean_fixation_points[:, 1], c=relative_times_fix, cmap='cool', s=100, alpha=1)
+    # Plot saccade arrows (no scatter points)
+    saccade_indices = saccade_data['saccadeindices']
+    start_points = np.array([gaze_data[start] for start, _ in saccade_indices])
+    end_points = np.array([gaze_data[end - 1] for _, end in saccade_indices])
+    relative_times_sacc = np.array([start / len(gaze_data) for start, _ in saccade_indices])
+    for i in range(len(start_points)):
+        ax.arrow(start_points[i, 0], start_points[i, 1], 
+                 end_points[i, 0] - start_points[i, 0], 
+                 end_points[i, 1] - start_points[i, 1], 
+                 head_width=0.05, color=plt.cm.viridis(relative_times_sacc))
 
 
 def overlay_roi_rects(ax, roi_rects):
-    # Overlay ROI rects (e.g., face, eyes)
+    # Overlay ROI rects with bottom-left and top-right corners
     for roi_name, roi_coords in roi_rects.items():
-        rect = plt.Rectangle((roi_coords[0], roi_coords[1]), roi_coords[2] - roi_coords[0], 
-                             roi_coords[3] - roi_coords[1], fill=False, edgecolor='red', linewidth=2)
+        # roi_coords should have [x_min, y_min, x_max, y_max]
+        rect = plt.Rectangle((roi_coords[0], roi_coords[1]), 
+                             roi_coords[2] - roi_coords[0], 
+                             roi_coords[3] - roi_coords[1], 
+                             fill=False, edgecolor='red', linewidth=2)
         ax.add_patch(rect)
+
