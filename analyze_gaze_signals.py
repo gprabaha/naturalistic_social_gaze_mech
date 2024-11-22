17721b444e94b8cd4977c3fd9fd3d82c1a342b3a
Prabaha <prabaha.gangopadhyay@yale.edu>
2024-11-22 15:44:43 -0500

    debug bitwise or calculation across multiple saccade timelines for cases where is from, say, face, but the to is 'any'

--- analyze_gaze_signals.py
+++ analyze_gaze_signals.py
@@ -37,7 +37,7 @@ def main():
         'remake_synchronized_gaze_data_df': False,
 
         'remake_fix_and_sacc': False,
-        'try_using_single_run': True,
+        'try_using_single_run': False,
         'recompute_fix_and_saccades_through_hpc_jobs': True,
         'hpc_job_output_subfolder': 'single_run_fix_saccade_results',
         
