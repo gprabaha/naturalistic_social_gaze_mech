a216ecd40c5d49b6b5c7435fbe74dbc1211a22da
John Lazzari <lazzarijohn2@gmail.com>
2025-09-26 14:45:08 -0400

    adding multiple model analysis

--- modeling/experiments/linear_analysis.py
+++ modeling/experiments/linear_analysis.py
@@ -70,6 +70,63 @@ def _get_max_eigenvalues(model_path, condition, *args, stim_inp=None):
     return max_eigs
 
 
+
+
+def _get_max_eigs_ablation(model_path, *args):
+    
+    hp = load_hp(model_path)
+    
+    dataset = get_mean_fixation_data("/Users/John/naturalistic_social_gaze_mech/social_gaze") 
+
+    model = Model(
+        hp["mrnn_config_file"], 
+        100, 
+        dataset.units_per_region["dmpfc"], 
+        dataset.units_per_region["accg"], 
+        dataset.units_per_region["ofc"], 
+        dataset.units_per_region["bla"], 
+        hp["dt"], 
+        hp["tau"], 
+        hp["inp_noise"], 
+        hp["act_noise"], 
+        hp["constrained"], 
+        hp["batch_first"],
+        hp["spectral_radius"],
+        device="cpu"
+    )
+    
+    checkpoint = torch.load(os.path.join(hp["save_dir"], hp["model_save_name"] + ".pth"))
+    model.load_state_dict(checkpoint)
+    model.eval()
+
+    batch, keys, _ = dataset.sample_batch()
+    inp = interactivity_input(keys, batch.shape[1])
+
+    regions = []
+    for region in model.mrnn.region_dict:
+        if region not in args:
+            regions.append(region)
+
+    stim_list = []
+    for s in range(len(regions)):
+        stim_list.append(stim_inp(
+            model.mrnn, 
+            50, 
+            100,
+            inp.shape[1],
+            0,
+            -5,
+            inp.shape[0], 
+            5,
+            10,
+            regions[s]
+        ))
+    
+    return stim_list, regions
+    
+
+
+
 def plot_max_eigs_pfc(model_path):
 
     exp_path = "results/linear_analysis"
@@ -128,53 +185,12 @@ def plot_max_eigs_ofc(model_path):
 
 
 
+
 # ABLATION
 def plot_max_eigs_pfc_ablation(model_path):
 
     exp_path = "results/linear_analysis"
-    hp = load_hp(model_path)
-    
-    dataset = get_mean_fixation_data("/Users/John/naturalistic_social_gaze_mech/social_gaze") 
-
-    model = Model(
-        hp["mrnn_config_file"], 
-        100, 
-        dataset.units_per_region["dmpfc"], 
-        dataset.units_per_region["accg"], 
-        dataset.units_per_region["ofc"], 
-        dataset.units_per_region["bla"], 
-        hp["dt"], 
-        hp["tau"], 
-        hp["inp_noise"], 
-        hp["act_noise"], 
-        hp["constrained"], 
-        hp["batch_first"],
-        hp["spectral_radius"],
-        device="cpu"
-    )
-    
-    checkpoint = torch.load(os.path.join(hp["save_dir"], hp["model_save_name"] + ".pth"))
-    model.load_state_dict(checkpoint)
-    model.eval()
-
-    batch, keys, _ = dataset.sample_batch()
-    inp = interactivity_input(keys, batch.shape[1])
-
-    regions = ["bla", "acc", "ofc"]
-    stim_list = []
-    for s in range(3):
-        stim_list.append(stim_inp(
-            model.mrnn, 
-            50, 
-            100,
-            inp.shape[1],
-            0,
-            -5,
-            inp.shape[0], 
-            5,
-            10,
-            regions[s]
-        ))
+    stim_list, regions = _get_max_eigs_ablation(model_path, "pfc")
     
     for s in range(3):
         fig, ax = standard_2d_ax()
@@ -192,49 +208,7 @@ def plot_max_eigs_pfc_ablation(model_path):
 def plot_max_eigs_acc_ablation(model_path):
 
     exp_path = "results/linear_analysis"
-    hp = load_hp(model_path)
-    
-    dataset = get_mean_fixation_data("/Users/John/naturalistic_social_gaze_mech/social_gaze") 
-
-    model = Model(
-        hp["mrnn_config_file"], 
-        100, 
-        dataset.units_per_region["dmpfc"], 
-        dataset.units_per_region["accg"], 
-        dataset.units_per_region["ofc"], 
-        dataset.units_per_region["bla"], 
-        hp["dt"], 
-        hp["tau"], 
-        hp["inp_noise"], 
-        hp["act_noise"], 
-        hp["constrained"], 
-        hp["batch_first"],
-        hp["spectral_radius"],
-        device="cpu"
-    )
-    
-    checkpoint = torch.load(os.path.join(hp["save_dir"], hp["model_save_name"] + ".pth"))
-    model.load_state_dict(checkpoint)
-    model.eval()
-
-    batch, keys, _ = dataset.sample_batch()
-    inp = interactivity_input(keys, batch.shape[1])
-
-    regions = ["bla", "pfc", "ofc"]
-    stim_list = []
-    for s in range(3):
-        stim_list.append(stim_inp(
-            model.mrnn, 
-            50, 
-            100,
-            inp.shape[1],
-            0,
-            -5,
-            inp.shape[0], 
-            5,
-            10,
-            regions[s]
-        ))
+    stim_list, regions = _get_max_eigs_ablation(model_path, "acc")
     
     for s in range(3):
         fig, ax = standard_2d_ax()
@@ -252,49 +226,7 @@ def plot_max_eigs_acc_ablation(model_path):
 def plot_max_eigs_bla_ablation(model_path):
 
     exp_path = "results/linear_analysis"
-    hp = load_hp(model_path)
-    
-    dataset = get_mean_fixation_data("/Users/John/naturalistic_social_gaze_mech/social_gaze") 
-
-    model = Model(
-        hp["mrnn_config_file"], 
-        100, 
-        dataset.units_per_region["dmpfc"], 
-        dataset.units_per_region["accg"], 
-        dataset.units_per_region["ofc"], 
-        dataset.units_per_region["bla"], 
-        hp["dt"], 
-        hp["tau"], 
-        hp["inp_noise"], 
-        hp["act_noise"], 
-        hp["constrained"], 
-        hp["batch_first"],
-        hp["spectral_radius"],
-        device="cpu"
-    )
-    
-    checkpoint = torch.load(os.path.join(hp["save_dir"], hp["model_save_name"] + ".pth"))
-    model.load_state_dict(checkpoint)
-    model.eval()
-
-    batch, keys, _ = dataset.sample_batch()
-    inp = interactivity_input(keys, batch.shape[1])
-
-    regions = ["pfc", "acc", "ofc"]
-    stim_list = []
-    for s in range(3):
-        stim_list.append(stim_inp(
-            model.mrnn, 
-            50, 
-            100,
-            inp.shape[1],
-            0,
-            -5,
-            inp.shape[0], 
-            5,
-            10,
-            regions[s]
-        ))
+    stim_list, regions = _get_max_eigs_ablation(model_path, "bla")
     
     for s in range(3):
         fig, ax = standard_2d_ax()
@@ -312,49 +244,7 @@ def plot_max_eigs_bla_ablation(model_path):
 def plot_max_eigs_ofc_ablation(model_path):
 
     exp_path = "results/linear_analysis"
-    hp = load_hp(model_path)
-    
-    dataset = get_mean_fixation_data("/Users/John/naturalistic_social_gaze_mech/social_gaze") 
-
-    model = Model(
-        hp["mrnn_config_file"], 
-        100, 
-        dataset.units_per_region["dmpfc"], 
-        dataset.units_per_region["accg"], 
-        dataset.units_per_region["ofc"], 
-        dataset.units_per_region["bla"], 
-        hp["dt"], 
-        hp["tau"], 
-        hp["inp_noise"], 
-        hp["act_noise"], 
-        hp["constrained"], 
-        hp["batch_first"],
-        hp["spectral_radius"],
-        device="cpu"
-    )
-    
-    checkpoint = torch.load(os.path.join(hp["save_dir"], hp["model_save_name"] + ".pth"))
-    model.load_state_dict(checkpoint)
-    model.eval()
-
-    batch, keys, _ = dataset.sample_batch()
-    inp = interactivity_input(keys, batch.shape[1])
-
-    regions = ["bla", "acc", "pfc"]
-    stim_list = []
-    for s in range(3):
-        stim_list.append(stim_inp(
-            model.mrnn, 
-            50, 
-            100,
-            inp.shape[1],
-            0,
-            -5,
-            inp.shape[0], 
-            5,
-            10,
-            regions[s]
-        ))
+    stim_list, regions = _get_max_eigs_ablation(model_path, "ofc")
     
     for s in range(3):
         fig, ax = standard_2d_ax()
@@ -368,6 +258,63 @@ def plot_max_eigs_ofc_ablation(model_path):
         ax.axvline(x=50, linestyle="--", color="grey", linewidth=2)
         ax.axvline(x=100, linestyle="--", color="grey", linewidth=2)
         save_fig(os.path.join(exp_path, f"ofc_max_eigs_all_conds_ablate_{regions[s]}"), eps=True)
+
+
+
+
+
+def plot_max_eigs_ablation_all_models_pfc():
+    
+    model_paths = [
+        "checkpoints/social_mrnn",
+        "checkpoints/social_mrnn_0",
+        "checkpoints/social_mrnn_1",
+        "checkpoints/social_mrnn_2",
+        "checkpoints/social_mrnn_3",
+        "checkpoints/social_mrnn_4",
+        "checkpoints/social_mrnn_5",
+    ]
+
+    max_eigs_high_int = {}
+    max_eigs_low_int = {}
+    max_eigs_object = {}
+
+    regions_ablate = ["acc", "ofc", "bla"]
+    for region in regions_ablate:
+        max_eigs_high_int[region] = []
+        max_eigs_low_int[region] = []
+        max_eigs_object[region] = []
+
+    for model_path in model_paths:
+        stim_list, regions = _get_max_eigs_ablation(model_path, "pfc")
+        
+        for s in range(3):
+            max_eigs_high_int[regions[s]].append(_get_max_eigenvalues(model_path, 0, "pfc", stim_inp=stim_list[s]))
+            max_eigs_low_int[regions[s]].append(_get_max_eigenvalues(model_path, 1, "pfc", stim_inp=stim_list[s]))
+            max_eigs_object[regions[s]].append(_get_max_eigenvalues(model_path, 2, "pfc", stim_inp=stim_list[s]))
+
+    fig, ax = standard_2d_ax()
+    for region in regions_ablate:
+        for run in range(len(model_paths)):
+            ax.plot(max_eigs_high_int[region][run], linewidth=2, color="red", alpha=0.25)
+            ax.fill_between(
+                x,
+                mean - std,
+                mean + std,
+                color="C0",
+                alpha=0.3,
+                label="±1 std",
+            )
+            ax.plot(max_eigs_low_int[region][run], linewidth=2, color="blue", alpha=0.25)
+            ax.plot(max_eigs_object[region][run], linewidth=2, color="green", alpha=0.25)
+        ax.set_title(f"Ablate {region} to pfc")
+        ax.axvline(x=50, linestyle="--", color="grey", linewidth=2)
+        ax.axvline(x=100, linestyle="--", color="grey", linewidth=2)
+        save_fig(os.path.join("results/linear_analysis", f"pfc_max_eigs_all_conds_ablate_{region}_all_models"), eps=True)
+
+
+
+
 
 def main():
     
